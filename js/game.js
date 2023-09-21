34ba1f7daf8deeccb1062328ece7461a716c5d29
Wences81 <zhylinpochta@gmail.com>
2023-09-21 17:32:09 +0200

    change3

--- js/game.js
+++ js/game.js
@@ -37,13 +37,20 @@ function direction(event) {
         dir = 'down';
 }
 
+function eatTail(head, arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (head.x == arr[i].x && head.y == arr[i].y)
+            clearInterval(game);    
+    }
+}
+
 function drawGame() {
     ctx.drawImage(ground, 0, 0);
 
     ctx.drawImage(foodImg, food.x, food.y);
 
     for (let i = 0; i < snake.length; i++) {
-        ctx.fillStyle = 'green';
+        ctx.fillStyle = i == 0 ? 'green' : 'red';
         ctx.fillRect(snake[i].x, snake[i].y, box, box);
     }
 
@@ -64,6 +71,10 @@ function drawGame() {
         snake.pop();
     }
 
+    if (snakeX < box || snakeX > box * 17
+        || snakeY < 3 * box || snakeY > box * 17)
+        clearInterval(game)
+
 
     if (dir == 'left') snakeX -= box;
     if (dir == 'right') snakeX += box;
@@ -74,6 +85,8 @@ function drawGame() {
         x: snakeX,
         y: snakeY
     };
+
+    eatTail(newHead, snake);
 
     snake.unshift(newHead);
 }
